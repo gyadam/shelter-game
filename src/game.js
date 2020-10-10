c8dedd1e735f94e2617a775af2216c11379c857c
adam-ubuntu <gy.adam90@gmail.com>
2020-10-09 21:16:40 -0700

    Add gameover screen & show player health

--- src/game.js
+++ src/game.js
@@ -6,6 +6,7 @@ import Player from './player.js';
 const GAMESTATE = {
     PAUSED: 0,
     RUNNING: 1,
+    GAMEOVER: 2
 }
 
 export default class Game{
@@ -37,7 +38,7 @@ export default class Game{
     togglePause(){
         if (this.gameState == GAMESTATE.RUNNING){
             this.gameState = GAMESTATE.PAUSED;
-        } else{
+        } else if (this.gameState == GAMESTATE.PAUSED){
             this.gameState = GAMESTATE.RUNNING;
         }
     }
@@ -57,11 +58,17 @@ export default class Game{
             this.particles.push(particle);
         }
 
-        if (this.gameState == GAMESTATE.RUNNING){
+        if (this.gameState != GAMESTATE.PAUSED){
             for (let particle of this.particles){
                 particle.update(deltaTime);
             }
+        }
+        if (this.gameState == GAMESTATE.RUNNING){
             this.player.update(deltaTime);
+        }
+
+        if (this.player.health <= 0){
+            this.gameState = GAMESTATE.GAMEOVER;
         }
     }
 
@@ -70,7 +77,9 @@ export default class Game{
             particle.draw(ctx);
         }
         this.house.draw(ctx);
-        this.player.draw(ctx);
+        if (this.gameState != GAMESTATE.GAMEOVER){
+            this.player.draw(ctx);
+        }
         
         if (this.gameState == GAMESTATE.PAUSED){
             ctx.fillStyle = 'rgba(0,0,0,0.4)';
@@ -80,6 +89,20 @@ export default class Game{
             ctx.textAlign = "center";
             ctx.fillText("Paused", this.gameWidth / 2, this.gameHeight / 3);
         }
+
+        if (this.gameState == GAMESTATE.GAMEOVER){
+            ctx.fillStyle = 'rgba(0,0,0,0.4)';
+            ctx.fillRect(0,0,this.gameWidth, this.gameHeight);
+            ctx.font = "bold 40px Arial";
+            ctx.fillStyle = "white";
+            ctx.textAlign = "center";
+            ctx.fillText("GAME OVER!", this.gameWidth / 2, this.gameHeight / 3);
+        }
+
+        ctx.font = "bold 40px Arial";
+        ctx.fillStyle = "black";
+        ctx.textAlign = "center";
+        ctx.fillText(this.player.health, 0.9 * this.gameWidth, 0.95 * this.gameHeight);
     }
 
 }
